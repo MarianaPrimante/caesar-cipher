3e9fb49af66a534e60942cbcac346435c68afb4c
MarianaPrimante <maprimante@gmail.com>
2019-01-23 10:26:38 -0200

    Inserir botões e caixas de texto

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,18 @@
 window.cipher = {
-  // ... 
+    // ... 
 };
+
+function pegaText() {
+    var text = document.getElementById("textTest").value;
+    var textCript = document.getElementById("textCipher");
+    var numDesl = document.getElementById("textNum");
+
+    textCript.innerHTML = ("O texto criptografado é " + text + ".");
+}
+
+function pegaTextTwo() {
+    var textTwo = document.getElementById("textTest").value;
+    var textDescript = document.getElementById("textCipherTwo");
+
+    textDescript.innerHTML = ("O texto descriptografado é " + textTwo + ".");
+}
