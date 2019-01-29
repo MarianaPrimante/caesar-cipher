bea2be958289d03666bf8ce811c6bc3043bc101c
MarianaPrimante <maprimante@gmail.com>
2019-01-29 17:15:56 -0200

    testes e começo css

--- src/cipher.js
+++ src/cipher.js
@@ -1,42 +1,15 @@
-function cifrar() {
-    const text = document.getElementById("text").value;
-    const numDesl = document.getElementById("textNum").value;
-    const numDeslOrig = parseInt(numDesl);
-    let letter = [];
-    let letterCipher = [];
-    let letterCipherEnd = "";
-    for (let i = 0; i < text.length; i++) {
-        letter.push(text.charCodeAt(i))
-    }
+function encode() {
+    let text = document.getElementById("text").value;
+    let offset = parseInt(document.getElementById("textNum").value);
 
-    for (let j = 0; j < letter.length; j++) {
-        console.log(letter[j])
-        let letterDesl = ""
-        if (letter[j] >= 65 && letter[j] <= 90) {
-            letterDesl = (((letter[j] - 65) + numDeslOrig) % 26) + 65
-        } else if (letter[j] >= 97 && letter[j] <= 122) {
-            letterDesl = (((letter[j] - 97) + numDeslOrig) % 26) + 97
-        } else if(letter[j]=32){
-            letterDesl = (letter[j]=32)
-          }
-        letterCipher.push(letterDesl)
-
-    }
-    for (let h = 0; h < letterCipher.length; h++) {
-        let letterConv = String.fromCharCode(letterCipher[h])
-        letterCipherEnd = letterCipherEnd + letterConv
-    }
-    document.getElementById("textCipher").innerHTML = ("O texto criptografado é " + letterCipherEnd + ".");
-
+    document.getElementById("textResp").innerHTML = cipher(text, offset);
 }
 
-function decifrar(){
-    const text = document.getElementById("textTwo").value;
-    const numDesl = document.getElementById("textNumTwo").value;
-    const numDeslOrig = parseInt(numDesl);
+function cipher(text, offset) {
     let letter = [];
     let letterCipher = [];
     let letterCipherEnd = "";
+
     for (let i = 0; i < text.length; i++) {
         letter.push(text.charCodeAt(i))
     }
@@ -45,12 +18,12 @@ function decifrar(){
         console.log(letter[j])
         let letterDesl = ""
         if (letter[j] >= 65 && letter[j] <= 90) {
-            letterDesl = (((letter[j] - 90) - numDeslOrig) % 26) + 90
+            letterDesl = (((letter[j] - 65) + offset) % 26) + 65
         } else if (letter[j] >= 97 && letter[j] <= 122) {
-            letterDesl = (((letter[j] - 122) - numDeslOrig) % 26) + 122
-        } else if(letter[j]=32){
-            letterDesl = (letter[j]=32)
-          }
+            letterDesl = (((letter[j] - 97) + offset) % 26) + 97
+        } else if (letter[j] = 32) {
+            letterDesl = (letter[j] = 32)
+        }
         letterCipher.push(letterDesl)
 
     }
@@ -58,5 +31,39 @@ function decifrar(){
         let letterConv = String.fromCharCode(letterCipher[h])
         letterCipherEnd = letterCipherEnd + letterConv
     }
-    document.getElementById("textCipherTwo").innerHTML = ("O texto descriptografado é " + letterCipherEnd + ".");
+    return letterCipherEnd
+}
+
+function decode() {
+    let text = document.getElementById("textTwo").value;
+    let offset = parseInt(document.getElementById("textNumTwo").value);
+
+    document.getElementById("textRespDec").innerHTML = cipherTwo(text, offset);
+}
+
+function cipherTwo(text, offset) {
+    let letter = [];
+    let letterCipher = [];
+    let letterCipherEnd = "";
+    for (let i = 0; i < text.length; i++) {
+        letter.push(text.charCodeAt(i))
+    }
+
+    for (let j = 0; j < letter.length; j++) {
+        let letterDesl = ""
+        if (letter[j] >= 65 && letter[j] <= 90) {
+            letterDesl = (((letter[j] - 90) - offset) % 26) + 90
+        } else if (letter[j] >= 97 && letter[j] <= 122) {
+            letterDesl = (((letter[j] - 122) - offset) % 26) + 122
+        } else {
+            letterDesl = letter[j]
+        }
+        letterCipher.push(letterDesl)
+
+    }
+    for (let h = 0; h < letterCipher.length; h++) {
+        let letterConv = String.fromCharCode(letterCipher[h])
+        letterCipherEnd = letterCipherEnd + letterConv
+    }
+    return letterCipherEnd
 }
---
+++ src/tests.js
@@ -0,0 +1,6 @@
+var result = cipher("abc", 2)
+if (result === "bcd") {
+    console.log("certo");
+} else {
+    console.log("errado")
+}
