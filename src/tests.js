a147e3389e4ff4c208275f5dddc4e6773de96266
MarianaPrimante <maprimante@gmail.com>
2019-01-31 18:20:31 -0200

    css pronto e diagrama

--- src/tests.js
+++ src/tests.js
@@ -25,3 +25,11 @@ function verificaIgualdade(deslocado, esperado) {
 }
 verificaIgualdade(cipher("xyz", 2), "zab")
 verificaIgualdade(cipher("AEIOU", 5), "ABCDE")
+verificaIgualdade(cipher("123", 5), "123")
+verificaIgualdade(cipher("b c d", 2), "d e f")
+
+
+verificaIgualdade(cipherTwo("zab", 2), "xyz")
+verificaIgualdade(cipherTwo("AEIOU", 5), "ABCDE")
+verificaIgualdade(cipherTwo("123", 5), "123")
+verificaIgualdade(cipherTwo("d e f", 2), "b c d")
